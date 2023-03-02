9fa2aff61617f53c5e18cfa5d682d91c3f718f8c
Hannu <varahahmo1@gmail.com>
2023-03-02 04:25:56 +0200

    POST captions to chatgpt && secrets

--- src/popup/main.js
+++ src/popup/main.js
@@ -1,3 +1,5 @@
+import secrets from "./secrets.js"; // find a logical way to do this in web extension way
+
 const button = document.getElementById('generator-btn');
 
 button.addEventListener('click', async () => {
@@ -14,4 +16,19 @@ button.addEventListener('click', async () => {
     let captions = await captionsData.text()
     captions = captions.replace(/<[^>]*>/g, '').replace(/&amp;#39;/g, '')
     console.log(captions)
+    console.log("captions length", captions.length)
+
+    const response = await fetch("https://api.openai.com/v1/chat/completions", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${secrets.APIKEY}`,
+        },
+        body: JSON.stringify({ "model": "gpt-3.5-turbo", "messages": [{ role: "user", content: `bullet point this: ${captions}` }] })
+    })
+    const data = await response.json()
+
+    console.log(data)
+    console.log(data.choices[0].message.content)
+
 })
