b22776f6edbc188ee18897c5c8bca37e2fb5c63e
Hannu <varahahmo1@gmail.com>
2023-03-07 23:09:16 +0200

    theme toggler for options

--- src/options/options.js
+++ src/options/options.js
@@ -1,3 +1,5 @@
+const themeToggle = document.querySelector("#theme-selector")
+
 function saveOptions(e) {
     e.preventDefault();
     browser.storage.sync.set({
@@ -18,5 +20,28 @@ function restoreOptions() {
     getting.then(setCurrentChoice, onError);
 }
 
+function toggleTheme() {
+    browser.storage.sync.set({
+        theme: themeToggle.checked
+    })
+}
+
+function restoreTheme() {
+    function setToggle(result) {
+        themeToggle.checked = result.theme
+    }
+
+    function logError(error) {
+        console.log(`Error: ${error}`);
+    }
+
+    let themeValue = browser.storage.sync.get("theme");
+    themeValue.then(setToggle, logError);
+}
+
+themeToggle.addEventListener("click", toggleTheme);
+
+document.querySelector("#theme-selector").addEventListener("click", toggleTheme);
 document.addEventListener("DOMContentLoaded", restoreOptions);
+document.addEventListener("DOMContentLoaded", restoreTheme);
 document.querySelector("form").addEventListener("submit", saveOptions);
