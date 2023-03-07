e82f4f33ff30af4ee342628215e5d74193a5a6a9
Hannu <varahahmo1@gmail.com>
2023-03-07 23:09:27 +0200

    gets theme and applies it

--- src/popup/index.js
+++ src/popup/index.js
@@ -1,5 +1,8 @@
 import { getCaptions } from './captions.js';
 import { generateResponse } from './chatgpt.js';
+const { theme } = await browser.storage.sync.get("theme")
+if (theme) document.body.classList.add("white-theme")
+
 
 async function main() {
     const captions = await getCaptions()
