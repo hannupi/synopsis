3d04f54d620340a0cda6a5a1a82965799c97bad0
Hannu <varahahmo1@gmail.com>
2023-03-27 19:02:47 +0300

    error handling for missing captions

--- src/popup/payload.js
+++ src/popup/payload.js
@@ -6,23 +6,30 @@ const sourceURL = source[0].url
 async function getContent() {
     switch (true) {
         case (/youtube/.test(sourceURL)):
-            const sourceData = await fetch(sourceURL, {
-                headers: {
-                    "Content-Type": "text/plain",
-                },
-            })
-            const sourceHTML = await sourceData.text()
+            try {
 
-            const regex = /({"captionTracks":.*isTranslatable":(true|false)}])/;
-            const [match] = regex.exec(sourceHTML);
-            const { captionTracks } = JSON.parse(`${match}}`);
-            // in case there are multiple languages, you can use the languageCode to find the one you want
-            const engCaptions = captionTracks.find((track) => track.languageCode === "en")
+                const sourceData = await fetch(sourceURL, {
+                    headers: {
+                        "Content-Type": "text/plain",
+                    },
+                })
+                const sourceHTML = await sourceData.text()
 
-            const captionsData = await fetch(engCaptions.baseUrl)
-            let captions = await captionsData.text()
-            captions = captions.replace(/<[^>]*>/g, '').replace(/&amp;#39;/g, '')
-            return captions;
+                const regex = /({"captionTracks":.*isTranslatable":(true|false)}])/;
+                const [match] = regex.exec(sourceHTML);
+                const { captionTracks } = JSON.parse(`${match}}`);
+                // in case there are multiple languages, you can use the languageCode to find the one you want
+                const engCaptions = captionTracks.find((track) => track.languageCode === "en")
+
+                const captionsData = await fetch(engCaptions.baseUrl)
+                let captions = await captionsData.text()
+                captions = captions.replace(/<[^>]*>/g, '').replace(/&amp;#39;/g, '')
+                return captions;
+            } catch (e) {
+                error.innerHTML = "<p>No captions found for this URL</p>"
+                loader.style.display = "none"
+                return
+            }
 
         default:
             const textContent = await browser.tabs.executeScript({
